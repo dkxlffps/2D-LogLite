26f83812ca24658da2556eba5cb1debe735821ad
Atila <dkxlffps@naver.com>
2020-06-04 00:05:41 +0900

    Jump Fiexd & GUI Cursor Sample
    - 점프 수정
    - 마우스 커서 샘플 추가

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,26 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class Player_Controller : MonoBehaviour
 {
     public float maxSpeed = 5f;
     public float jumpPower = 5f;
 
+    public Boolean isGround;
+
     Rigidbody2D rigidId ;
     SpriteRenderer spriteId;
+
+    Vector3 mousePosition;
+    Camera mainCamera;
+
+    public float rayCastLength = 1.35f;
 
     void Awake() {
         rigidId = GetComponent<Rigidbody2D>();
         spriteId = GetComponent<SpriteRenderer>();
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(rigidId.position, Vector3.down , rayCastLength, LayerMask.GetMask("Field"));
+        if (hit.collider != null)
+            isGround = true;
+        else
+            isGround = false;
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
+        }
+
         //Jump
         if (Input.GetButtonDown("Jump"))
-            rigidId.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        {
+            if (isGround)
+            {
+                rigidId.velocity = Vector2.zero;
+              rigidId.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+        }
+
 
         if (Input.GetButtonUp("Horizontal"))
         {
@@ -32,7 +57,7 @@ public class Player_Controller : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
+    {   
         //Move
         float h = Input.GetAxisRaw("Horizontal");
         rigidId.AddForce(Vector2.right * h, ForceMode2D.Impulse);
